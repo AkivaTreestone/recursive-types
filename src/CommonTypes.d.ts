14af11a1f8c4ef5387a11082a866e4671e3591a8
Akiva Silver <akiva@treestone.com>
2021-04-28 17:14:54 -0400

    Added docs for common types

--- src/CommonTypes.d.ts
+++ src/CommonTypes.d.ts
@@ -1,3 +1,10 @@
+/**
+ * Create a type alias if recursion depth limit is reached.
+ * Use this if you want to check if your type is too recursive.
+ */
 export type RecursionLimit = unknown;
 
+/**
+ * Target any function
+ */
 export type AnyFunc = (...args: any) => any;
